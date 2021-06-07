c779894dd2c2286f55d2ce1e60f6b79349c5bd59
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-06-07 11:34:15 +0500

    commit2

--- app.js
+++ app.js
@@ -28,25 +28,21 @@
 //************************** Task : 03 ********************** */
 
 
-function toFindArea(a,b,c){
-
-    // var sum = a+b+c
-    var S = ( a + b + c ) / 2
-
-    var area = S* (S-a)*(S-b)*(S-c)
-document.write("area of triangle is : "+ area)
-
- }
- var side1 = +prompt("Enter a :");
- var side2 = +prompt("Enter b :");
- var side3 = +prompt("Enter c :");
-
- toFindArea(side1,side2,side3)
+// function toFindArea(a,b,c){
 
 
-//  function toFindS(){
+//     var S = ( a + b + c ) / 2
+
+//     var area = S* (S-a)*(S-b)*(S-c)
+// document.write("area of triangle is : "+ area)
 
 //  }
+//  var side1 = +prompt("Enter a :");
+//  var side2 = +prompt("Enter b :");
+//  var side3 = +prompt("Enter c :");
+
+//  toFindArea(side1,side2,side3)
+
 
 
 //************************** Task : 04 ********************** */
@@ -77,3 +73,139 @@ document.write("area of triangle is : "+ area)
 
 
 
+//************************** Task : 05 ********************** */
+
+// function indexOf(word) {
+//     var text = "The quick brown fox jumps over the lazy dog."
+//     var flag = false;
+//     for (var i = 0; i < text.length; i++) {
+//         if (word == text.slice(i, i + word.length)) {
+//             flag = true
+//             document.write("Text is : " + text + "<br> Index of "+ word+ " is : "  + text.indexOf(word))
+//             break
+//         }
+
+//     }
+//     if(flag == flase){
+//         document.write("word not match")
+//     }
+
+// }
+
+// var userInput = prompt("Which word's index of text you want to know\nText is : The quick brown fox jumps over the lazy dog.")
+// indexOf(userInput)
+
+
+//************************** Task : 06 ********************** */
+
+// function removeVowel(sentence){
+
+//    document.write(sentence.replace(/[aeiou]/g,""))
+// }
+
+// var userInput = prompt("Enter your sentence less than 26 words").toLowerCase();
+// removeVowel(userInput)
+
+
+
+//************************** Task : 07 ********************** */
+
+// function findArr(text) {
+//     var arr = ["a", "e", "i", "o", "u"]
+//     var arr2 = ["a", "e", "i", "o", "u"]
+
+// var flag = false
+
+//     for (var a = 0; a < text.length; a++) {
+//     for (var i = 0; i < arr.length; i++) {
+//         for (var j = 0; j < arr2.length; j++) {
+//                 if (text.slice(a, a + 2) === arr[i] + arr2[j]) {
+//                     flag = true
+//                     document.write(text.slice(a, a + 2)+" found at index number "+ a+"<br>")
+//                     break
+//                 }
+//             }
+//         }
+//         if(flag=false){
+//             document.write("no two vowels in succession in a line of text found")
+//         }
+//     }
+// }
+
+// var userInput = prompt("Enter your sentence to find two vowels in a word","Pleases read this application and give me gratuity");
+// findArr(userInput)
+
+
+
+//************************** Task : 08 ********************** */
+
+// function convrtInMtr(km){
+// var meters = km*1000
+// return meters
+// }
+// function convrtInFt(km){
+//     var feet = km*3280.84
+//     return feet
+//     }
+// function convrtInInch(km){
+//     var inches = km*39370.1
+//     return inches
+//     }
+// function convrtInCm(km){
+//     var centimtr = km*100000
+//     return centimtr
+//     }
+
+
+// var userInput = prompt("Enter distance between two cities")
+
+// function convert(calc){
+// var kilometer = calc
+// document.write("kilometer : "+ kilometer+"<br> meter : " + convrtInMtr(kilometer)
+// +"<br> feet : " +convrtInFt(kilometer)+"<br> inches : " +convrtInInch(kilometer)
+// +"<br> centimeter : " +convrtInCm(kilometer))
+// }
+// convert(userInput)
+
+
+//************************** Task : 09 ********************** */
+
+
+// function calcPay(){
+// var overHours = 40;
+// var rateHour = 12.00;
+// var totalPay = overHours*rateHour
+
+// document.write("overtime pay of employees for "+overHours+" hours Rs."+totalPay)
+// }
+
+// calcPay()
+
+
+//************************** Task : 10 ********************** */
+
+// function amount(value) {
+
+//     if (value.length === 3) {
+
+//         document.write("You will have " + value.charAt(0) + " hundred notes ")
+
+//         if (value.charAt(1) > 4) {
+
+//             document.write(1 + " fifty note ")
+//         }
+
+//         if (value.charAt(1) > 5) {
+//             document.write(value.charAt(1) - 5 + " ten notes.")
+//         }
+//         else if (value.charAt(1) > 0 && value.charAt(1) < 5) {
+//             document.write(value.charAt(1) + " ten notes.")
+//         }
+
+//     } else { alert("Please enter amount in hundreds, i.e.100-990") }
+
+
+// }
+
+// var userInput = prompt("Enter amount to withdrawn in hundred", "500")
+// amount(userInput)
